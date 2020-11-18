d9a88b780b081cb13a0c0a5a4659ed54c9027ea9
joao <joaovtolente@gmail.com>
2020-11-18 09:46:16 -0300

    Tabuada

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira um valor a ser multiplicado");
+            int num1 = int.Parse(Console.ReadLine());
+
+            int contador = 0;
+            int tabela = num1*10; //Cálculo da tabuada
+
+            while (contador <= tabela)
+            {
+                Console.WriteLine("-----------------------");
+                Console.WriteLine(contador);
+                contador = contador+1 * num1; //Multiplicação
+            }
+            
+        }
+    }
+}
